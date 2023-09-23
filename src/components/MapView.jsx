dd76e16a81693a0ac5c1ac808f2089e29a6a0e12
Carl0sPineda <carlos7712p@gmail.com>
2023-09-23 10:13:54 -0600

    fixed margin right

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,15 +1,10 @@
-import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 
 const MapView = () => {
   return (
     <>
       {/** RENDER */}
-      <MapContainer
-        className="render d-flex justify-content-center align-items-center"
-        center={[10.63504, -85.43772]}
-        zoom={9}
-      >
+      <MapContainer className="render" center={[10.63504, -85.43772]} zoom={9}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </MapContainer>
     </>
