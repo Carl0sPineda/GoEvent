53df2c2740dc17ae21ee048be11371426fff5c88
Carl0sPineda <carlos7712p@gmail.com>
2023-10-29 22:11:15 -0600

    change lat and lng center

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -55,7 +55,7 @@ const MapView = () => {
   };
 
   return (
-    <MapContainer className="render" center={[10.63504, -85.43772]} zoom={9}>
+    <MapContainer className="render" center={[10.46701, -84.96775]} zoom={9}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {data.map((e) => (
         <Marker
